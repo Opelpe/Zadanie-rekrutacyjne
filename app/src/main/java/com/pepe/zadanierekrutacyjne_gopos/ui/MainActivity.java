c8920d8b13a558032290a940feac2575df5ef9f0
Opelpe <91432013+Opelpe@users.noreply.github.com>
2022-07-05 22:40:27 +0200

    First commit

---
+++ app/src/main/java/com/pepe/zadanierekrutacyjne_gopos/ui/MainActivity.java
@@ -0,0 +1,18 @@
+package com.pepe.zadanierekrutacyjne_gopos.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pepe.zadanierekrutacyjne_gopos.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
